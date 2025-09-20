444594328aa69116982a9f2d6668de915bda79dc
ganjingdemo <ganjingdemo@gmail.com>
2025-09-21 07:31:30 +0800

    Add bash file to do build.

---
+++ 01_setup_build_env.sh
@@ -0,0 +1,25 @@
+#https://github.com/mumble-voip/mumble/blob/1.4.x/docs/dev/build-instructions/build_linux.md
+
+sudo apt install \
+  build-essential \
+  cmake \
+  pkg-config \
+  qttools5-dev \
+  qttools5-dev-tools \
+  libqt5svg5-dev \
+  libboost-dev \
+  libssl-dev \
+  libprotobuf-dev \
+  protobuf-compiler \
+  libprotoc-dev \
+  libcap-dev \
+  libxi-dev \
+  libasound2-dev \
+  libogg-dev \
+  libsndfile1-dev \
+  libspeechd-dev \
+  libavahi-compat-libdnssd-dev \
+  libzeroc-ice-dev \
+  libpoco-dev \
+  g++-multilib
+
---
+++ 03_gen_makefile.sh
@@ -0,0 +1,1 @@
+qmake -recursive main.pro CONFIG+="release no-elevation no-g15 no-asio" CONFIG-=sse2
